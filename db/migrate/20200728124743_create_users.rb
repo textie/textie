f8e291f115d66d23a72610d36be7aa6c91505517
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-07-28 16:01:10 +0300

    create user

---
+++ db/migrate/20200728124743_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    enable_extension("citext")
+
+    create_table :users do |t|
+      t.citext :email, null: false
+      t.string :full_name
+      t.string :password_digest
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
