4f6001b1dd3924aed2853288c7f0e356ff6bacc9
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-07-28 17:01:25 +0300

    generate empty controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  module :api do
+    module :v1 do
+    end
+  end
 end
