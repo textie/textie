f2fc1ef01a17650e693a8b8ed7feb8842011b7f9
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-08-14 17:11:07 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :courses
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :users, only: %i[create]
       resources :sessions, only: %i[create]
       resource :profile, only: %i[show]
+
+      resources :courses
     end
   end
 end
