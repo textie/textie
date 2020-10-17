80d12d43a0724e9584f4f3b9bf2bf07b1338412b
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-10-17 18:33:15 +0300

    update profile spec

--- spec/acceptance/profiles_request_spec.rb
+++ spec/acceptance/profiles_request_spec.rb
@@ -1,13 +1,12 @@
 require "rails_helper"
+require "rspec_api_documentation/dsl"
 
-RSpec.describe "Api::V1::Profiles", type: :request do
+RSpec.resource "Api::V1::Profiles" do
   include_context "with API request"
 
-  describe "GET /api/v1/profile" do
+  get "/api/v1/profile" do
     let(:user) { create(:user) }
     let(:jwt) { LoginUser::GenerateJwt.call(user: user).token }
-    let(:headers) { { "Authorization" => "JWT #{jwt}" } }
-    let(:do_request) { get "/api/v1/profile", headers: headers }
     let(:user_attributes) do
       {
         "id" => user.id,
@@ -16,16 +15,19 @@ RSpec.describe "Api::V1::Profiles", type: :request do
       }
     end
 
-    it "returns current user's profile" do
-      expect(response_body).to match("user" => user_attributes)
+    context "with valid authentication" do
+      before { header "Authorization", "JWT #{jwt}" }
+
+      example_request "returns current user's profile" do
+        expect(response_body).to match_json("user" => user_attributes)
+      end
     end
 
     context "with invalid authentication" do
-      let(:headers) { { "Authentication" => "xyz" } }
+      header "Authorization", "xyz"
 
-      it "response with unauthorized status" do
-        do_request
-        expect(response).to be_unauthorized
+      example_request "response with unauthorized status" do
+        expect(status).to eq(401)
       end
     end
   end
