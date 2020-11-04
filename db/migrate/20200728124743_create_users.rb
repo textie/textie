4a16bbfbbb4923acd1f6f2d3ece1c7a01cf89a63
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-11-04 13:35:04 +0300

    fix constraints in migrations

--- db/migrate/20200728124743_create_users.rb
+++ db/migrate/20200728124743_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.citext :email, null: false
       t.string :full_name, null: false, default: ""
-      t.string :password_digest
+      t.string :password_digest, null: false
 
       t.timestamps
     end
