23859fc2d9a38f3e89fb67334e27f3607eb5da8f
Dmitry Barskov <dmitry.barskov@flatstack.com>
2020-11-04 10:39:35 +0300

    Update spec/factories/users.rb
    Co-authored-by: Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user, aliases: [:author] do
+  factory :user do
     sequence(:email) { |n| "user_#{n}@example.com" }
     full_name { Faker::Name.name }
     password { "123456" }
